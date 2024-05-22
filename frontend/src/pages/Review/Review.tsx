5702534d2f857554de385ca0ff17529fe9e2c916
Jon Gamble <jonathan.gamble@palmetto.com>
2024-05-22 01:07:35 -0400

    unflatten the code doc array on review page

--- frontend/src/pages/Review/Review.tsx
+++ frontend/src/pages/Review/Review.tsx
@@ -15,45 +15,114 @@ function Review() {
     id: 1,
     content: [
       {
+        id: '86Rf07',
         type: "h1",
         text: "header 1",
         children: [
           {
+            id: 'XRKUdQ',
             type: 'p',
-            text: 'p1'
+            text: 'p1',
+            path: '86Rf07'
           },
           {
+            id: '0liwDd',
             type: "h2", 
             text: 'header 2', 
+            path: '86Rf07',
             children: [
               {
+                id: 'sPW92uE',
                 type: "h3",
                 text: 'header 3',
+                path: '86Rf07.0liwDd',
                 children: [
-                  { type: 'p', text: 'text 2' },
-                  { type: 'p', text: 'text 3' },
-                  { type: 'p', text: 'text 4' },
-                  { type: 'p', text: 'text 5' },
+                  { id: 'QAe51FC', type: 'p', text: 'text 2', path: '86Rf07.0liwDd.sPW92uE' },
+                  { id: 'CsPW92u', type: 'p', text: 'text 3', path: '86Rf07.0liwDd.sPW92uE' },
+                  { id: '92uEOyq', type: 'p', text: 'text 4', path: '86Rf07.0liwDd.sPW92uE' },
+                  { id: '6Sp8YzV', type: 'p', text: 'text 5', path: '86Rf07.0liwDd.sPW92uE' },
                 ]
               }
             ]
           },
           {
+            id: 'YzVTmnU',
             type: "h2",
             text: 'header 4',
+            path: '86Rf07',
             children: [
-              { type: 'p', text: 'text 6' },
-              { type: 'p', text: 'text 7' },
-              { type: 'p', text: 'text 8' },
+              { id: 'iwDdHXL', type: 'p', text: 'text 6', path: '86Rf07.YzVTmnU' },
+              { id: 'jZrfxNh', type: 'p', text: 'text 7', path: '86Rf07.YzVTmnU' },
+              { id: 'dHXLajZ', type: 'p', text: 'text 8', path: '86Rf07.YzVTmnU' },
             ]
           },
           {
+            id: 'Bg6Sp8Yz',
             type: "h2",
-            text: 'header 5'
+            text: 'header 5',
+            path: '86Rf07'
           },
           {
+            id: 'yq4Bg6S',
             type: "h2",
-            text: 'header 6'
+            text: 'header 6',
+            path: '86Rf07',
+          },
+        ]
+      },
+      {
+        id: 'test2',
+        type: "h1",
+        text: "header 1",
+        children: [
+          {
+            id: 'XRKUdQ',
+            type: 'p',
+            text: 'p1',
+            path: 'test2'
+          },
+          {
+            id: '0liwDd',
+            type: "h2", 
+            text: 'header 2', 
+            path: 'test2',
+            children: [
+              {
+                id: 'sPW92uE',
+                type: "h3",
+                text: 'header 3',
+                path: 'test2.0liwDd',
+                children: [
+                  { id: 'QAe51FC', type: 'p', text: 'text 2', path: 'test2.0liwDd.sPW92uE' },
+                  { id: 'CsPW92u', type: 'p', text: 'text 3', path: 'test2.0liwDd.sPW92uE' },
+                  { id: '92uEOyq', type: 'p', text: 'text 4', path: 'test2.0liwDd.sPW92uE' },
+                  { id: '6Sp8YzV', type: 'p', text: 'text 5', path: 'test2.0liwDd.sPW92uE' },
+                ]
+              }
+            ]
+          },
+          {
+            id: 'YzVTmnU',
+            type: "h2",
+            text: 'header 4',
+            path: 'test2',
+            children: [
+              { id: 'iwDdHXL', type: 'p', text: 'text 6', path: 'test2.YzVTmnU' },
+              { id: 'jZrfxNh', type: 'p', text: 'text 7', path: 'test2.YzVTmnU' },
+              { id: 'dHXLajZ', type: 'p', text: 'text 8', path: 'test2.YzVTmnU' },
+            ]
+          },
+          {
+            id: 'Bg6Sp8Yz',
+            type: "h2",
+            text: 'header 5',
+            path: 'test2'
+          },
+          {
+            id: 'yq4Bg6S',
+            type: "h2",
+            text: 'header 6',
+            path: 'test2',
           },
         ]
       },
@@ -82,7 +151,7 @@ function Review() {
   };
 
   const addDocBlock = () => {
-    setDocBlocks([...docBlocks, {type: "h1", text: ""}]);
+    setDocBlocks([...docBlocks, {id: "test", type: "h1", text: ""}]);
     reset();
   };
 
--- frontend/src/utils/transformCodeDoc.ts
+++ frontend/src/utils/transformCodeDoc.ts
@@ -12,8 +12,10 @@ export type CodeDoc = {
 }
 
 export type CodeDocBlock = {
+  id: string;
   type: string;
   text: string;
+  path?: string;
   children?: CodeDocBlock[];
 }
 
@@ -22,7 +24,7 @@ export const flattenCodeDoc = (codeDoc:CodeDocBlock[]) => {
 
   // Helper function to traverse the object
   function traverse(currentObj: CodeDocBlock) {
-      result.push({ type: CodeDocBlockType[currentObj.type], text: currentObj.text });
+      result.push({ id: currentObj.id, type: CodeDocBlockType[currentObj.type], text: currentObj.text, path: currentObj.path });
       
       if ('children' in currentObj && Array.isArray(currentObj.children)) {
           currentObj.children.forEach(child => traverse(child));
@@ -38,35 +40,26 @@ export const flattenCodeDoc = (codeDoc:CodeDocBlock[]) => {
 }
 
 export const reshapeCodeDoc = (flattenedCodeDoc: CodeDocBlock[]) => {
-  let result: CodeDocBlock[] = [], prevResult;
+  const resultObj = flattenedCodeDoc.reduce((acc, val) => {
+    const key = val.path || 'prime';
+    acc[key] = {...acc[key], [val.id]: val};
+    return acc;
+  }, {})
 
-  flattenedCodeDoc.forEach((docBlock) =>{
-    if(!prevResult) {
-      result.push({type: docBlock.type, text: docBlock.text, children: []});
+  Object.keys(resultObj).forEach((val) => {
+    const keySplit = val.split('.');
+    const lastKey = keySplit.pop();
+    if(keySplit.length > 0) {
+      if(lastKey) {
+        resultObj[keySplit.join('.')][lastKey].children = Object.keys(resultObj[val]).map(subVal => resultObj[val][subVal]);
+      } 
     } else {
-      if (headerRank(prevResult.type) < headerRank(docBlock.type)) {
-        result[(result.length-1)].children?.push(docBlock)
-      } else {
-        result.push(docBlock);
+      if (lastKey !== 'prime') {
+        resultObj['prime'][lastKey].children = Object.keys(resultObj[val]).map(subVal => resultObj[val][subVal]);
       }
     }
-    if (docBlock.type !== 'p') {
-      prevResult = docBlock;
-    }
-  })
-}
+  });
 
-const headerRank = (header) => {
-  switch (header) {
-    case 'h1':
-      return 1;
-    case 'h2':
-      return 2;
-    case 'h3':
-      return 3;
-    case 'h4':
-      return 4;
-    default:
-      return 5;
-  }
+  const result = Object.keys(resultObj['prime']).map(key => resultObj['prime'][key]);
+  return result;
 }
